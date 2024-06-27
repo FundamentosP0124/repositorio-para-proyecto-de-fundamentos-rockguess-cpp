67c647a4529a6171949bd545e6a32cbb313b02de
JuanBustillo <128838789+JuanBustillo@users.noreply.github.com>
2024-06-27 12:30:40 -0600

    subiendo funcion para imprimir el proseso del jugador

--- RockGuess.cpp
+++ RockGuess.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-
-// Función para mostrar el menú y que permitira que el jugador elija una banda
-string banda() {
+// Función para mostrar el menú y permitir que el jugador elija una banda
+string Banda() {
     cout << "Elige una banda:" << endl;
     cout << "1. Guns N' Roses" << endl;
     cout << "2. Megadeth" << endl;
     cout << "3. Poison" << endl;
-    cout << "4, Salir del menu" << endl;
+    cout << "4. Salir del menú" << endl;
     int option;
     cin >> option;
-
     switch (option) {
         case 1:
             return "Guns N' Roses";
@@ -25,11 +22,29 @@ string banda() {
             return "Guns N' Roses";
     }
 }
-
+// Función para imprimir el progreso actual del jugador de manera básica
+void printProgress(const string& songName, const string& progress) {
+    cout << "Progreso: ";
+    for (int i = 0; i < songName.length(); ++i) {
+        if (progress[i] != '_') {
+            cout << progress[i] << " ";
+        } else {
+            cout << "_ ";
+        }
+    }
+    cout << endl;
+}
 int main() {
-    string band = banda();
+    string songName = "Sweet Child O' Mine";
+    string progress(songName.length(), '_');
+    printProgress(songName, progress);
+    string band = chooseBand();
+    cout << "Has elegido la banda: " << band << endl;
 
-    // Aquí continuaremos con el código.
+    // Aquí puedes continuar con el código según lo necesites.
+    // Por ejemplo, podrías llamar a chooseRandomSong(band) y luego usar printProgress().
+   //aqui continuaremos con las funciones secundarias
+   //por ejemplo una que llame a la funcion welcomememsaje que haremos dentro de poco 
 
     return 0;
 }
